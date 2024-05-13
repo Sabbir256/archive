311b278d9af0cd1eac023a50dc0e8876db7be152
Md. Sabbir Ahmed <29686831+Sabbir256@users.noreply.github.com>
2024-05-13 23:58:23 +0600

    added icons

---
+++ src/icons/index.ts
@@ -0,0 +1,7 @@
+export * from "./IconGithub";
+export * from "./IconLinkedIn";
+export * from "./IconX";
+export * from "./IconInstagram";
+export * from "./IconArrowUpRight";
+export * from "./IconLeetCode";
+export * from "./IconArrowRight";
